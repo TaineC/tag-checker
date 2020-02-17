0ae7b2432109226001b9c355f50797f0ad5e1ec0
Taine Collins <taine.collins.1999@gmail.com>
2020-02-17 14:49:21 +1300

    basic js checker

---
+++ javascript.js
@@ -0,0 +1,52 @@
+let htmlText = checkHTML();
+
+console.log(htmlText)
+
+function checkHTML(){
+    let doc = document.getElementById('body');
+    let text = doc.innerHTML;
+    return text;
+}
+
+validateHTML = (htmlText) => {
+    let htmlValidationRegExp = /<.+?>/g;
+    if(htmlText.length == 0){
+        return false;
+    }
+    else{
+        var text =  htmlText.match(htmlValidationRegExp);
+        return text;
+    }
+}
+
+function checkArray(){
+    let tagArray = validateHTML(htmlText)
+    console.log(tagArray)
+
+    const arrWithSlashElements = tagArray.filter(e => e.includes('/'));
+    const arrWithoutSlashElements = tagArray.filter(e => !e.includes('/'));
+    console.log(arrWithSlashElements)
+    console.log(arrWithoutSlashElements)
+
+    if(arrWithSlashElements.length == arrWithoutSlashElements.length){
+        console.log(true)
+    }
+    else{
+        console.log(false)
+    }
+
+    //future code
+
+    //various conditions calculated based upon array sort methods
+    // - reverse sort match array string to other array string
+
+}
+
+checkArray();
+
+// function printedResult(){
+//     var result = ('testing ' + htmlValidationRegExp)
+//     return result;
+// }
+
+// document.getElementById('result').innerHTML = printedResult();
